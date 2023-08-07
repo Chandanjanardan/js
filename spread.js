a539f2641a0f2b190a3e4da47ad0feae1903e720
Chandan Janardan <chandanjanardan@gmail.com>
2023-08-07 07:27:53 +0530

    adding spread array

---
+++ spread.js
@@ -0,0 +1,9 @@
+const colors=["red","orange","yellow"]
+
+function giveMeOutput(a,b,c,d){
+    console.log("a",a)
+    console.log("b",b)
+    console.log("c",c)
+    console.log("d",d)
+}
+console.log(giveMeOutput(...colors))
